9c7c398279fbfcf9e8648f8ef1fb2ed3312ec7df
CarlosArriagaCM <CarlosArriagaCM@gmail.com>
2016-08-12 09:45:48 -0500

    minor changes

--- init/init_hwY635.c
+++ init/init_hwY635.c
@@ -80,28 +80,12 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.product", "Y635-L03");
         property_set("ro.telephony.default_network", "9");
     }
-    /* Y550-L01 */
-   else if (strstr(model, "Y550-L01") != NULL) {
-        property_set("ro.product.model", "Y550-L01");
-        property_set("ro.product.name", "Y550-L01");
-        property_set("ro.product.device", "Y550-L01");
-        property_set("ro.build.product", "Y550-L01");
-        property_set("ro.telephony.default_network", "9");
-    }
-    /* Y550-L02 */
-   else if (strstr(model, "Y550-L02") != NULL) {
-        property_set("ro.product.model", "Y550-L02");
-        property_set("ro.product.name", "Y550-L02");
-        property_set("ro.product.device", "Y550-L02");
-        property_set("ro.build.product", "Y550-L02");
-        property_set("ro.telephony.default_network", "7");
-    }
-    /* Y550-L03 */
-   else if (strstr(model, "Y550-L03") != NULL) {
-        property_set("ro.product.model", "Y550-L03");
-        property_set("ro.product.name", "Y550-L03");
-        property_set("ro.product.device", "Y550-L03");
-        property_set("ro.build.product", "Y550-L03");
+    /* Y635-L21 */
+   else if (strstr(model, "Y635-L21") != NULL) {
+        property_set("ro.product.model", "Y635-L21");
+        property_set("ro.product.name", "Y635-L21");
+        property_set("ro.product.device", "Y635-L21");
+        property_set("ro.build.product", "Y635-L21");
         property_set("ro.telephony.default_network", "9");
     }
 }
